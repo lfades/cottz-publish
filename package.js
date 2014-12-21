956f4b791aa3d566e5f8500b3a8da86238824753
luiskotsuki <luisito453@gmail.com>
2014-12-21 12:46:05 -0500

    v 2.0.1

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Edit the documents to your liking before sending",
-  version: "2.0.0",
+  version: "2.0.1",
   git: "https://github.com/Goluis/meteor-publish-with-relations.git"
 });
 
